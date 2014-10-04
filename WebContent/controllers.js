75ba8bf762cb06b215c29ebe023a66dd3b056c94
Krzysztof <krzysztof.telka@gmail.com>
2014-10-04 16:39:22 +0200

    module Angular JS

--- WebContent/controllers.js
+++ WebContent/controllers.js
@@ -1,31 +1,7 @@
-var messages = {};
-messages.someText = 'Application based on Angular JS! :)';
+var myAppModule = angular.module('myApp', []);
 
-function TextController($scope) {
-	$scope.messages = messages;
-}
-
-function HelloController($scope) {
-	$scope.greeting = {
-		text : 'Hello'
-	};
-}
-
-function CartController($scope) {
-	$scope.items = [ {
-		title : 'Paint pots',
-		quantity : 8,
-		price : 3.95
-	}, {
-		title : 'Polka dots',
-		quantity : 17,
-		price : 12.95
-	}, {
-		title : 'Pebbles',
-		quantity : 5,
-		price : 6.95
-	} ];
-	$scope.remove = function(index) {
-		$scope.items.splice(index, 1);
-	}
-}
+myAppModule.controller('TextController', function($scope) {
+	var someText = {};
+	someText.message = "Uruchomiłeś aplikację Angular JS";
+	$scope.someText = someText;
+});
--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,15 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html ng-app>
+<html ng-app='myApp'>
 <head>
 <script type="text/javascript" src="angular.js"></script>
 <script type="text/javascript" src="controllers.js"></script>
-<!-- <script type="text/javascript" src="jquery.js"></script> -->
-<!-- <script type="text/javascript" src="skrypt.js"></script> -->
 <title>Angular</title>
 </head>
 <body ng-controller="TextController">
-	<p>{{messages.someText}}</p>
+	<p>{{someText.message}}</p>
 </body>
 </html>
